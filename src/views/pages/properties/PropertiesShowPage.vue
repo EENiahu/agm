4da536128fd785cd88cabe83062278c58618046f
qMaDMaNp <medifman@gmail.com>
2021-08-04 20:58:33 +0300

    Add properties remove

--- src/views/pages/properties/PropertiesShowPage.vue
+++ src/views/pages/properties/PropertiesShowPage.vue
@@ -75,9 +75,47 @@
                     </router-link>
                   </v-col>
                   <v-col class="flex-grow-0">
-                    <v-btn type="button" class="px-10" color="red darken-2 white--text" depressed rounded>
-                      Remove
-                    </v-btn>
+                    <v-menu
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        offset-y
+                        :nudge-bottom="8"
+                        :offset-overflow="true"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn v-on="on" v-bind="attrs" type="button" class="px-10" color="red darken-2 white--text" depressed rounded>
+                          Remove
+                        </v-btn>
+                      </template>
+
+                      <v-card>
+                        <v-card-title>
+                          Are you sure?
+                        </v-card-title>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+
+                          <v-btn type="button" class="px-5" color="blue-grey darken-4 white--text" outlined depressed rounded
+                              @click="menu = false"
+                          >
+                            No
+                          </v-btn>
+                          <v-btn
+                              rounded
+                              class="px-5"
+                              color="red"
+                              text
+                              type="button"
+                              @click="removeProperty(property.id)"
+                          >
+                            Remove
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-menu>
                   </v-col>
                 </v-row>
               </v-expansion-panel-content>
@@ -95,6 +133,10 @@
     name: "PropertiesShowPage",
     data() {
       return {
+        fav: true,
+        menu: false,
+        message: false,
+        hints: true,
         properties: [],
       }
     },
@@ -113,6 +155,27 @@
               console.error(err)
             })
       },
+
+      removeProperty(id) {
+        apiProperties.delete(id)
+          .then(res => {
+            this.menu = false;
+            this.removeObjectById(this.properties, id);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+      },
+
+      removeObjectById(arr, id) {
+        let index = null;
+
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].id === id) index = i;
+        }
+
+        if (index != null) arr.splice(index, 1);
+      }
     }
   }
 </script>
