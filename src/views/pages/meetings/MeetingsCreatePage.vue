58c9a913df6a92acf757a78109b717cd721c2382
qMaDMaNp <medifman@gmail.com>
2021-08-09 17:03:22 +0300

    Add show errors meeting edit and create

--- src/views/pages/meetings/MeetingsCreatePage.vue
+++ src/views/pages/meetings/MeetingsCreatePage.vue
@@ -31,6 +31,8 @@
       <v-row>
         <v-col cols="2">
           <v-select
+              @change="handleInput('PropertyId')"
+              :error-messages="errors.get('PropertyId')"
               v-model="inputs.meeting.PropertyId"
               name="PropertyId"
               hide-details="auto"
@@ -46,6 +48,8 @@
       <v-row>
         <v-col cols="4">
           <v-text-field
+              @input="handleInput('Title')"
+              :error-messages="errors.get('Title')"
               v-model="inputs.meeting.Title"
               name="Title"
               color="orange"
@@ -58,6 +62,8 @@
       <v-row>
         <v-col cols="4">
           <v-textarea
+              @input="handleInput('Description')"
+              :error-messages="errors.get('Description')"
               v-model="inputs.meeting.Description"
               name="Description"
               color="orange"
@@ -71,6 +77,8 @@
       <v-row>
         <v-col cols="2">
           <v-select
+              @change="handleInput('TimeZoneId')"
+              :error-messages="errors.get('TimeZoneId')"
               v-model="inputs.meeting.TimeZoneId"
               name="TimeZoneId"
               hide-details="auto"
@@ -91,8 +99,8 @@
             <template v-slot:activator="{ on, attrs }">
               <v-text-field v-model="inputs.dateFrom" @click:append="menuDateFrom = !menuDateFrom" readonly color="orange"
                   name="StartDateTime" label="Start Date" append-icon="mdi-calendar" hide-details="auto"
-                  v-bind="attrs" v-on="on"
-              ></v-text-field>
+                  v-bind="attrs" v-on="on" :error="!!errors.get('StartDateTime')">
+              </v-text-field>
             </template>
             <v-date-picker v-model="inputs.dateFrom" color="orange" type="date" flat no-title scrollable>
               <v-spacer></v-spacer>
@@ -103,33 +111,13 @@
         </v-col>
 
         <v-col cols="2">
-          <v-menu ref="menuDateTo" v-model="menuDateTo" color="orange"
-                  :close-on-content-click="false" :return-value.sync="inputs.dateTo"
-                  transition="scale-transition" offset-y max-width="290px" min-width="auto">
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field v-model="inputs.dateTo" @click:append="menuDateTo = !menuDateTo" readonly color="orange"
-                            name="EndDateTime" label="End Date" append-icon="mdi-calendar" hide-details="auto"
-                            v-bind="attrs" v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker v-model="inputs.dateTo" color="orange" type="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn text color="orange" @click="menuDateTo = false">Cancel</v-btn>
-              <v-btn text color="orange" @click="$refs.menuDateTo.save(inputs.dateTo)">OK</v-btn>
-            </v-date-picker>
-          </v-menu>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col cols="2">
           <v-menu ref="menuTimeFrom" v-model="menuTimeFrom" :close-on-content-click="false"
-              :nudge-right="40" :return-value.sync="inputs.timeFrom"
-              transition="scale-transition" offset-y max-width="290px" min-width="290px">
+                  :nudge-right="40" :return-value.sync="inputs.timeFrom"
+                  transition="scale-transition" offset-y max-width="290px" min-width="290px">
             <template v-slot:activator="{ on, attrs }">
               <v-text-field :value="timeFromTitle" @click:append="menuTimeFrom = !menuTimeFrom" readonly color="orange"
                             label="Start Time" append-icon="mdi-clock-time-four-outline" hide-details="auto"
-                            v-bind="attrs" v-on="on">
+                            v-bind="attrs" v-on="on" :error="!!errors.get('StartDateTime')">
               </v-text-field>
             </template>
             <v-time-picker v-if="menuTimeFrom" v-model="inputs.timeFrom" @click:minute="$refs.menuTimeFrom.save(inputs.timeFrom)"
@@ -140,6 +128,38 @@
             </v-time-picker>
           </v-menu>
         </v-col>
+      </v-row>
+
+      <v-row v-if="errors.has('StartDateTime')" no-gutters class="mt-2" transition="slide-y-transition">
+        <v-col cols="4" transition="slide-y-transition">
+          <div class="v-text-field__details">
+            <div class="v-messages theme--light error--text" role="alert">
+              <div class="v-messages__wrapper">
+                <div class="v-messages__message">{{ errors.get('StartDateTime') }}</div>
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="2">
+          <v-menu ref="menuDateTo" v-model="menuDateTo" color="orange"
+                  :close-on-content-click="false" :return-value.sync="inputs.dateTo"
+                  transition="scale-transition" offset-y max-width="290px" min-width="auto">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field v-model="inputs.dateTo" @click:append="menuDateTo = !menuDateTo" readonly color="orange"
+                            name="EndDateTime" label="End Date" append-icon="mdi-calendar" hide-details="auto"
+                            v-bind="attrs" v-on="on" :error="!!errors.get('EndDateTime')">
+              </v-text-field>
+            </template>
+            <v-date-picker v-model="inputs.dateTo" color="orange" type="date" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="orange" @click="menuDateTo = false">Cancel</v-btn>
+              <v-btn text color="orange" @click="$refs.menuDateTo.save(inputs.dateTo)">OK</v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-col>
 
         <v-col cols="2">
           <v-menu ref="menuTimeTo" v-model="menuTimeTo" :close-on-content-click="false"
@@ -148,7 +168,7 @@
             <template v-slot:activator="{ on, attrs }">
               <v-text-field :value="timeToTitle" @click:append="menuTimeTo = !menuTimeTo" readonly color="orange"
                             label="End Time" append-icon="mdi-clock-time-four-outline" hide-details="auto"
-                            v-bind="attrs" v-on="on">
+                            v-bind="attrs" v-on="on" :error="!!errors.get('EndDateTime')">
               </v-text-field>
             </template>
             <v-time-picker v-if="menuTimeTo" v-model="inputs.timeTo" @click:minute="$refs.menuTimeTo.save(inputs.timeTo)"
@@ -161,9 +181,23 @@
         </v-col>
       </v-row>
 
+      <v-row v-if="errors.has('EndDateTime')" no-gutters class="mt-2" transition="slide-y-transition">
+        <v-col cols="4" transition="slide-y-transition">
+          <div class="v-text-field__details">
+            <div class="v-messages theme--light error--text" role="alert">
+              <div class="v-messages__wrapper">
+                <div class="v-messages__message">{{ errors.get('EndDateTime') }}</div>
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+
       <v-row>
         <v-col cols="4">
           <v-text-field
+              @input="handleInput('MeetingRegistrationLink')"
+              :error-messages="errors.get('MeetingRegistrationLink')"
               v-model="inputs.meeting.MeetingRegistrationLink"
               name="TotalUnits"
               type="text"
@@ -182,11 +216,13 @@ import apiStates from "@/api/states";
 import apiTimezones from "@/api/timezones";
 import apiProperties from "@/api/properties";
 import apiMeetings from "@/api/meetings";
+import errorHandler from "@/lib/ErrorHandler";
 
 export default {
   name: "MeetingsCreatePage",
   data () {
     return {
+      errors: new errorHandler(),
       disabled: false,
       loading: false,
 
@@ -218,6 +254,24 @@ export default {
     }
   },
 
+  watch: {
+    'inputs.dateFrom'() {
+      this.errors.clear('StartDateTime');
+    },
+
+    'inputs.timeFrom'() {
+      this.errors.clear('StartDateTime');
+    },
+
+    'inputs.dateTo'() {
+      this.errors.clear('EndDateTime');
+    },
+
+    'inputs.timeTo'() {
+      this.errors.clear('EndDateTime');
+    },
+  },
+
   computed: {
     timeFromTitle() {
       if (!this.inputs.timeFrom.length) return '';
@@ -245,10 +299,12 @@ export default {
     },
 
     StartDateTime() {
+      if (!this.inputs.dateFrom || !this.inputs.timeFrom) return '';
       return this.inputs.dateFrom + ' ' + this.inputs.timeFrom + ':00';
     },
 
     EndDateTime() {
+      if (!this.inputs.dateTo || !this.inputs.timeTo) return '';
       return this.inputs.dateTo + ' ' + this.inputs.timeTo + ':00';
     },
   },
@@ -260,6 +316,11 @@ export default {
   },
 
   methods: {
+    handleInput(name) {
+      if (this.errors.has(name)) this.errors.clear(name);
+      if (this.errors.has('Message')) this.errors.clear('Message');
+    },
+
     deactivateSubmit() {
       this.disabled = true;
       this.loading = true;
@@ -276,7 +337,6 @@ export default {
 
       const meetingParams = {...this.inputs.meeting, StartDateTime: this.StartDateTime, EndDateTime: this.EndDateTime};
 
-      console.log(meetingParams);
       apiMeetings.create(meetingParams)
         .then(res => {
           this.activateSubmit();
@@ -284,6 +344,7 @@ export default {
         })
         .catch(err => {
           this.activateSubmit();
+          if (err.response && err.response.data.errors) this.errors.record(err.response.data.errors);
         })
     },
 
